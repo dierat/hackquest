bf74a0128304126456e1dacf021eb54a521f8559
dierat <diedra.rater@gmail.com>
2015-08-03 11:50:34 -0700

    removes sample code from intial meteor project

--- hackquest/hackquest.js
+++ hackquest/hackquest.js
@@ -3,9 +3,6 @@ Monsters = new Mongo.Collection('monsters');
 
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
   // Login requires username instead of e-mail address for easier testing.
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
@@ -15,19 +12,6 @@ if (Meteor.isClient) {
   Template.loginScreen.rendered = function() {
     Accounts._loginButtonsSession.set('dropdownVisible', true);
   };
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
