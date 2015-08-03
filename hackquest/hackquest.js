6358a1e0d2882aad17ebd83cb6c9045ef556cff9
dierat <diedra.rater@gmail.com>
2015-08-03 11:39:13 -0700

    adds main login page that is shown is user is not logged in

--- hackquest/hackquest.js
+++ hackquest/hackquest.js
@@ -2,6 +2,16 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  // Login requires username instead of e-mail address for easier testing.
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+  
+  // Renders the login panel as uncollapsed on login_page template.
+  Template.loginScreen.rendered = function() {
+    Accounts._loginButtonsSession.set('dropdownVisible', true);
+  };
+
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
