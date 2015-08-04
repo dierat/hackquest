c0839f8cb83387e1458a2de98245ce5fa29ad335
dierat <diedra.rater@gmail.com>
2015-08-03 20:50:50 -0700

    adds one working action for players to take

--- README.md
+++ README.md
@@ -11,36 +11,29 @@ Most basic features needed for version 1.0:
   <li><strike>list of all potential team members in a collection</strike></li>
   <li><strike>list of all potential monsters in a collection</strike></li>
   <li><strike>have a saved games collection to hold users current team, monsters, and level</strike></li>
+  <li><strike>have a button for starting a new game</strike></li>
   <li><strike>when the new game button is clicked, add new document to saved games for that user with 4 randomly selected team members</strike></li>
   <li><strike>list current messages in the game console</strike></li>
   <li><strike>list of current monsters in a collection and shown on screen (icon + name?)</strike></li>
   <li><strike>list current team members on screen (name, photo, and stamina)</strike></li>
-  <li>monsters should have an ability that reduces a characters' stamina</li>
-  <li>characters should have an ability that reduces a monster's health</li>
-  <li>the user should be able to choose which monster each of the current characters attack (each in order until the end of the list)</li>
+  <li>characters should have abilities that reduces a monster's health</li>
+  <li>after the player chooses an action for a character, the execution context should move to the next character in the list until it reaches the end</li>
   <li>if a monster's health is reduced to 0, it is removed from the current game</li>
-  <li>if no monsters are currently in the game, the player wins</li>
+  <li>if the last monster was defeated, the player wins</li>
+  <li>monsters should have an ability that reduces a characters' stamina</li>
   <li>when the player has finished choosing the actions of the characters, each monster should attack a random character</li>
   <li>if a character's stamina is reduced to 0, s/he is removed from the current team</li>
   <li>if there are no current team members, the player loses</li>
-  <li>after a game ends, show a win or lose message and then start a new game</li>
+  <li>after a game ends, show a win or lose message</li>
 </ul>
 
 Additional features for version 1.1:
 <ul>
   <li>have styling for which entity is current choosing/performing an action</li>
-  <li>track which level user is on, starting at 0</li>
-  <li>when all current monsters have been defeated, increase the user's level and (if they were not on the last level) copy the ids of the next level's monsters into their current monster collection (otherwise show win message and start new game)</li>
-  <li>have a button for starting a new game</li>
   <li>add more abilities to characters for the user to choose from</li>
   <li>add more abilities to monsters that the program chooses randomly</li>
   <li>add animations for who is acting and who is being acted upon</li>
   <li>add sound effects</li>
   <li>add music</li>
   <li>ability to choose team members when starting a new game</li>
-</ul>
-
-Pie in the sky goals for version 1.2:
-<ul>
-  <li>refactor UI to use React instead of Blaze</li>
 </ul>
--- hackquest/hackquest.js
+++ hackquest/hackquest.js
@@ -3,6 +3,14 @@ Monsters = new Mongo.Collection('monsters');
 Games = new Mongo.Collection('games');
 
 
+var findGame = function(){
+  // returns the player's game document from the Games collection
+  if (Games){
+    return Games.findOne({ userId: Meteor.userId() });
+  }
+};
+
+
 if (Meteor.isClient) {
   // Login requires username instead of e-mail address for easier testing.
   Accounts.ui.config({
@@ -14,6 +22,21 @@ if (Meteor.isClient) {
     Accounts._loginButtonsSession.set('dropdownVisible', true);
   };
 
+  var play = function(){
+    console.log("play calling!");
+    var gameId = findGame()._id;
+    var activeEntity = findGame().activeEntity;
+    if (findGame().playerTurn){
+      Games.update({_id: gameId}, {$set: 
+        {
+          messages: [
+            'It is now ' + activeEntity.name + "' turn!"
+          ]
+        }
+      });
+    }
+  };
+
   Template.main.events({
     'click .newGame': function(){
       // grab id's from the characters and monsters documents
@@ -22,7 +45,7 @@ if (Meteor.isClient) {
           id: c._id,
           name: c.name,
           icon: c.icon,
-          stam: c.stam
+          stam: c.stam,
         };
       });
       var monsters = Monsters.find().map(function(m){
@@ -31,11 +54,10 @@ if (Meteor.isClient) {
           name: m.name,
           icon: m.icon,
           stam: m.stam,
-          level: m.level
         };
       });
       // find and delete user's existing game if one exists
-      var currenGame = Games.findOne({userId: Meteor.userId()});
+      var currenGame = findGame();
       if (currenGame){
         var gameId = currenGame._id;
         Games.remove({_id: gameId});
@@ -45,25 +67,91 @@ if (Meteor.isClient) {
         userId: Meteor.userId(),
         level: 0,
         characters: team.slice(0,4),
-        monsters: monsters,
+        monster: monsters[0],
+        activeEntity: team[0],
+        playerTurn: true,
         messages: [
           "You've created a new game!",
           "Are you ready to battle the evil forces of web development?!!"
         ]
       });
+      setTimeout(function(){
+        play();
+      }, 1000);
     }
   });
 
   Template.gameScreen.helpers({
     messages: function(){
-      return Games.findOne({userId: Meteor.userId()}).messages;
+      return findGame().messages;
     },
-    monsters: function(){
-      return Games.findOne({userId: Meteor.userId()}).monsters;
+    monster: function(){
+      return findGame().monster;
     },
     characters: function(){
-      return Games.findOne({userId: Meteor.userId()}).characters;
+      return findGame().characters;
+    },
+    playerTurn: function(){
+      return findGame().playerTurn;
     }
+  });
+
+  Template.gameScreen.events({
+    'click .code': function(){
+      var code = Math.floor(Math.random() * 10) + 2;
+      console.log("You wrote " + code + " lines of code!");
+      // reduce stam of first monster in game array
+      var gameId = findGame()._id;
+      // find out new monster stam and if it's still alive
+      var monsterStam = findGame().monster.stam - code;
+      var newMonster = {};
+      if (monsterStam > 0){
+        // reconstruct monster object
+        newMonster = {
+          _id: findGame().monster._id,
+          name: findGame().monster.name,
+          stam: monsterStam,
+          icon: findGame().monster.icon
+        };
+        Games.update(
+          {_id: gameId},
+          {$set: 
+            // update monster with new object
+            {monster: newMonster, 
+            // update messages with last activity
+            messages: [ findGame().activeEntity.name + " wrote " + code + " lines of code!" ]}
+          }
+        );
+        console.log(findGame().monster.name + " now has " + monsterStam + "stamina!");
+      }
+
+    },
+    'click .google': function(){
+      // generate random # between 0 and 1
+      // if that number is greater than .25, it's a hit
+      var hit = Math.random() > 0.45;
+      if (hit){
+        console.log("it's a hit!");
+        var code = Math.floor(Math.random() * 15) + 15;
+        console.log("You wrote " + code + " lines of code!");
+      } else {
+        console.log("No useful results!");
+      }
+      // reduce stam of first monster in game array
+    },
+    'click .giphy': function(){
+      // generate random # between 0 and 1
+      // if that number is greater than .25, it's a hit
+      var hit = Math.random() > 0.85;
+      if (hit){
+        console.log("it's a hit!");
+        var buff = Math.floor(Math.random() * 7) + 15;
+        console.log("You increase your teammates stamina by " + buff + "!");
+      } else {
+        console.log("That didn't even make sense!");
+      }
+      // reduce stam of first monster in game array
+    },
   });
 }
 
