ebc42e8627111e475251da1cf8c78e18b67d5975
dierat <diedra.rater@gmail.com>
2015-08-04 12:27:08 -0700

    removes characters from current game when they've been defeated and shows a lose message to player when all characters have been removed

--- hackquest/helperFunctions.js
+++ hackquest/helperFunctions.js
@@ -31,25 +31,55 @@ monsterTurn = function(){
   var hit = Math.random() > 0.15;
   if (hit){
     // pick a random character
-    var ranIndex = Math.floor(Math.random() * 4);
+    var ranIndex = Math.floor(Math.random() * team.length);
     var target = team[ranIndex];
     // figure out how much damage it does
-    var damage = Math.floor(Math.random() * 13);
+    var damage = Math.floor(Math.random() * 30);
     // update target's stam
     target.stam -= damage;
-    team[ranIndex] = target;
     setTimeout(function(){
       Games.update({_id: gameId}, {$set: {
-        characters: team,
-        messages: [ monster.name + " did " + damage + " damage to " + target.name + "!" ]
+        messages: [ monster.name + " did " + damage + " damage to " + target.name + "'s stamina!" ]
       }});
     }, 1500);
+
+    // if target is still in the game
+    if (target.stam > 0){
+      team[ranIndex] = target;
+      setTimeout(function(){
+        Games.update({_id: gameId}, {$set: {
+          characters: team
+        }});
+      }, 1500);
+    // if the target has fallen
+    } else {
+      var fallen = target.name;
+      team.splice(ranIndex, 1)
+      setTimeout(function(){
+        Games.update({_id: gameId}, {$set: {
+          characters: team
+        }});
+        Games.update({_id: gameId}, {$addToSet: {
+          messages: fallen + " has been overcome by exhaustion!"
+        }});
+        // if the team has been defeated
+        if (!team.length){
+          Games.update({_id: gameId}, {$addToSet: {
+            messages: [
+              ["Your team has fallen to the toils of web development!"],
+              ["Hit 'New Game' to try again!"]
+            ],
+          }});
+        }
+      }, 1500);
+    }
   // if monster missed
   } else {
     setTimeout(function(){
       Games.update({_id: gameId},{$addToSet: {
         messages: monster.name + " is stunned by your awesomeness!"
       }});
+      play();
     }, 1500);
   }
   // pass execution back to player
