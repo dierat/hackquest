3f9135e8b97da5b23e7f8e811cb196531def79d1
dierat <diedra.rater@gmail.com>
2015-08-04 08:50:44 -0700

    slimmed it down to just have one action that writes code and kills monsters for now

--- hackquest/playerActions.js
+++ hackquest/playerActions.js
@@ -1,66 +1,23 @@
+
+
+
 if (Meteor.isClient){  
   Template.gameScreen.events({
+
     'click .code': function(){
       var gameId = findGame()._id;
-      // figure out damage done
-      var code = Math.floor(Math.random() * 10) + 2;
-      Games.update({_id: gameId}, {$set: {
-        messages: [ findGame().activeEntity.name + " wrote " + code + " lines of code!" ]
-      }});
+      var hit = Math.random() > 0.15;
 
-      // find out new monster stam and if it's still alive
-      var monsterStam = findGame().monster.stam - code;
-      var newMonster = {};
-      // if the monster is still alive
-      if (monsterStam > 0){
-        // reconstruct monster object
-        newMonster = {
-          _id: findGame().monster._id,
-          name: findGame().monster.name,
-          stam: monsterStam,
-          icon: findGame().monster.icon
-        };
-        // update monster with new object
-        Games.update({_id: gameId},{$set: {monster: newMonster}});
-        console.log(findGame().monster.name + " now has " + monsterStam + "stamina!");
-      } else {
-        console.log("monster has been killed!");
-        // find next monster, if there is one
-        var currentLevel = findGame().level;
-        console.log("currentLevel = ", currentLevel);
-        var nextMonster = Monsters.findOne({level: currentLevel + 1});
-        console.log("nextMonster = ", nextMonster);
-        if (nextMonster){
-          var newMonster = {
-            name: nextMonster.name,
-            stam: nextMonster.stam,
-            icon: nextMonster.icon
-          };
-          console.log("nextMonster = ", newMonster);
-          Games.update({_id: gameId}, {$set: {
-            monster: newMonster,
-            level: currentLevel + 1
-          }});
-        } else {
-          Games.update({_id: gameId}, {$set: {
-            messages: ["You won! Please hit the 'New Game' button to play again!"],
-            playerTurn: false
-          }});          
-        }
-      }
-    },
-    'click .google': function(){
-      var hit = Math.random() > 0.35;
-      var gameId = findGame()._id;
+      // if the action was successful
       if (hit){
-        console.log("it's a hit!");
-        var code = Math.floor(Math.random() * 15) + 15;
-        console.log("You wrote " + code + " lines of code!");
+        // figure out how much code was written and what the monster has left
+        var code = Math.floor(Math.random() * 15) + 5;
         var monsterStam = findGame().monster.stam - code;
-        var newMonster = {};
+
+        // if the monster is still alive
         if (monsterStam > 0){
-          // reconstruct monster object
-          newMonster = {
+          // update monster with new health
+          var newMonster = {
             _id: findGame().monster._id,
             name: findGame().monster.name,
             stam: monsterStam,
@@ -69,25 +26,133 @@ if (Meteor.isClient){
           Games.update(
             {_id: gameId},
             {$set: 
-              // update monster with new object
               {monster: newMonster, 
-              // update messages with last activity
               messages: [ findGame().activeEntity.name + " wrote " + code + " lines of code!" ]}
             }
           );
           console.log(findGame().monster.name + " now has " + monsterStam + "stamina!");
+
+        // if the monster is dead
+        } else {
+          // look for next monster
+          var currentLevel = findGame().level;
+          var nextMonster = Monsters.findOne({level: currentLevel + 1});
+
+          // if there is a next monster
+          if (nextMonster){
+            // update monster to be next monster
+            var newMonster = {
+              name: nextMonster.name,
+              stam: nextMonster.stam,
+              icon: nextMonster.icon
+            };
+            Games.update({_id: gameId}, {$set: {
+              monster: newMonster,
+              level: currentLevel + 1
+            }});
+
+          // if that was the last monster
+          } else {
+            // update game with win message
+            Games.update({_id: gameId}, {$set: {
+              messages: ["You won! Please hit the 'New Game' button to play again!"],
+              playerTurn: false
+            }});          
+          }
         }
+
+
+      // if the action was not successful
       } else {
-        console.log("No useful results!");
         Games.update(
           {_id: gameId},
           {$set: 
-            // update messages with last activity
-            {messages: [ "No useful results!" ]}
+            {messages: [ "You didn't write a single line!" ]}
           }
         );
       }
+      // figure out if that was the last character in the team
+      // if not
+        // update active entity to be next character
+      // else
+        // activate monsters' turn
     },
+
+    // 'click .google': function(){
+    //   var gameId = findGame()._id;
+    //   var hit = Math.random() > 0.35;
+
+    //   // if the action was successful
+    //   if (hit){
+    //     // figure out how much code was written and what the monster has left
+    //     var code = Math.floor(Math.random() * 15) + 15;
+    //     var monsterStam = findGame().monster.stam - code;
+
+    //     // if the monster is still alive
+    //     if (monsterStam > 0){
+    //       // update monster with new health
+    //       var newMonster = {
+    //         _id: findGame().monster._id,
+    //         name: findGame().monster.name,
+    //         stam: monsterStam,
+    //         icon: findGame().monster.icon
+    //       };
+    //       Games.update(
+    //         {_id: gameId},
+    //         {$set: 
+    //           {monster: newMonster, 
+    //           messages: [ findGame().activeEntity.name + " wrote " + code + " lines of code!" ]}
+    //         }
+    //       );
+    //       console.log(findGame().monster.name + " now has " + monsterStam + "stamina!");
+
+    //     // if the monster is dead
+    //     } else {
+    //       // look for next monster
+    //       var currentLevel = findGame().level;
+    //       var nextMonster = Monsters.findOne({level: currentLevel + 1});
+
+    //       // if there is a next monster
+    //       if (nextMonster){
+    //         // update monster to be next monster
+    //         var newMonster = {
+    //           name: nextMonster.name,
+    //           stam: nextMonster.stam,
+    //           icon: nextMonster.icon
+    //         };
+    //         Games.update({_id: gameId}, {$set: {
+    //           monster: newMonster,
+    //           level: currentLevel + 1
+    //         }});
+
+    //       // if that was the last monster
+    //       } else {
+    //         // update game with win message
+    //         Games.update({_id: gameId}, {$set: {
+    //           messages: ["You won! Please hit the 'New Game' button to play again!"],
+    //           playerTurn: false
+    //         }});          
+    //       }
+    //     }
+
+
+    //   // if the action was not successful
+    //   } else {
+    //     console.log("No useful results!");
+    //     Games.update(
+    //       {_id: gameId},
+    //       {$set: 
+    //         {messages: [ "No useful results!" ]}
+    //       }
+    //     );
+    //   }
+    //   // figure out if that was the last character in the team
+    //   // if not
+    //     // update active entity to be next character
+    //   // else
+    //     // activate monsters' turn
+    // },
+
     'click .giphy': function(){
       Games.update(
         {_id: findGame()._id},
@@ -96,6 +161,7 @@ if (Meteor.isClient){
           {messages: [ "giphy fail! Everyone laughed at " + findGame().activeEntity.name + "!" ]}
         }
       );
-    },
+    }
+
   });
 }
