d5d47f7dd9e2936ff6577dc39449bfffb295cfe4
dierat <diedra.rater@gmail.com>
2015-08-03 21:23:11 -0700

    players can now defeat bosses in sequence and win when they are all defeated

--- hackquest/hackquest.js
+++ hackquest/hackquest.js
@@ -98,13 +98,17 @@ if (Meteor.isClient) {
 
   Template.gameScreen.events({
     'click .code': function(){
-      var code = Math.floor(Math.random() * 10) + 2;
-      console.log("You wrote " + code + " lines of code!");
-      // reduce stam of first monster in game array
       var gameId = findGame()._id;
+      // figure out damage done
+      var code = Math.floor(Math.random() * 10) + 2;
+      Games.update({_id: gameId}, {$set: {
+        messages: [ findGame().activeEntity.name + " wrote " + code + " lines of code!" ]
+      }});
+
       // find out new monster stam and if it's still alive
       var monsterStam = findGame().monster.stam - code;
       var newMonster = {};
+      // if the monster is still alive
       if (monsterStam > 0){
         // reconstruct monster object
         newMonster = {
@@ -113,44 +117,82 @@ if (Meteor.isClient) {
           stam: monsterStam,
           icon: findGame().monster.icon
         };
-        Games.update(
-          {_id: gameId},
-          {$set: 
-            // update monster with new object
-            {monster: newMonster, 
-            // update messages with last activity
-            messages: [ findGame().activeEntity.name + " wrote " + code + " lines of code!" ]}
-          }
-        );
+        // update monster with new object
+        Games.update({_id: gameId},{$set: {monster: newMonster}});
         console.log(findGame().monster.name + " now has " + monsterStam + "stamina!");
+      } else {
+        console.log("monster has been killed!");
+        // find next monster, if there is one
+        var currentLevel = findGame().level;
+        console.log("currentLevel = ", currentLevel);
+        var nextMonster = Monsters.findOne({level: currentLevel + 1});
+        console.log("nextMonster = ", nextMonster);
+        if (nextMonster){
+          var newMonster = {
+            name: nextMonster.name,
+            stam: nextMonster.stam,
+            icon: nextMonster.icon
+          };
+          console.log("nextMonster = ", newMonster);
+          Games.update({_id: gameId}, {$set: {
+            monster: newMonster,
+            level: currentLevel + 1
+          }});
+        } else {
+          Games.update({_id: gameId}, {$set: {
+            messages: ["You won! Please hit the 'New Game' button to play again!"],
+            playerTurn: false
+          }});          
+        }
       }
-
     },
     'click .google': function(){
-      // generate random # between 0 and 1
-      // if that number is greater than .25, it's a hit
-      var hit = Math.random() > 0.45;
+      var hit = Math.random() > 0.35;
+      var gameId = findGame()._id;
       if (hit){
         console.log("it's a hit!");
         var code = Math.floor(Math.random() * 15) + 15;
         console.log("You wrote " + code + " lines of code!");
+        var monsterStam = findGame().monster.stam - code;
+        var newMonster = {};
+        if (monsterStam > 0){
+          // reconstruct monster object
+          newMonster = {
+            _id: findGame().monster._id,
+            name: findGame().monster.name,
+            stam: monsterStam,
+            icon: findGame().monster.icon
+          };
+          Games.update(
+            {_id: gameId},
+            {$set: 
+              // update monster with new object
+              {monster: newMonster, 
+              // update messages with last activity
+              messages: [ findGame().activeEntity.name + " wrote " + code + " lines of code!" ]}
+            }
+          );
+          console.log(findGame().monster.name + " now has " + monsterStam + "stamina!");
+        }
       } else {
         console.log("No useful results!");
+        Games.update(
+          {_id: gameId},
+          {$set: 
+            // update messages with last activity
+            {messages: [ "No useful results!" ]}
+          }
+        );
       }
-      // reduce stam of first monster in game array
     },
     'click .giphy': function(){
-      // generate random # between 0 and 1
-      // if that number is greater than .25, it's a hit
-      var hit = Math.random() > 0.85;
-      if (hit){
-        console.log("it's a hit!");
-        var buff = Math.floor(Math.random() * 7) + 15;
-        console.log("You increase your teammates stamina by " + buff + "!");
-      } else {
-        console.log("That didn't even make sense!");
-      }
-      // reduce stam of first monster in game array
+      Games.update(
+        {_id: findGame()._id},
+        {$set: 
+          // update messages with last activity
+          {messages: [ "giphy fail! Everyone laughed at " + findGame().activeEntity.name + "!" ]}
+        }
+      );
     },
   });
 }
